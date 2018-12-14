7fb6347b73f3d99e9516d4fc3ff60a4e0abbf5a5
Jason <jasond.campbell@gmail.com>
2018-12-13 21:28:20 -0800

    Updates

---
+++ precache-manifest.c4fcafc089a46e64224fdc956b00aa1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2f50dd021392f4abdc8",
+    "url": "/static/js/main.d2f50dd0.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "d2f50dd021392f4abdc8",
+    "url": "/static/css/main.f914be91.chunk.css"
+  },
+  {
+    "revision": "52e6486a9e498f369b06b21c5cb572a4",
+    "url": "/index.html"
+  }
+];
